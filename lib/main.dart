082b4c050c6be897a99aef11796c3cce0ce40e12
Zendard <thijsmuylaert@hotmail.com>
2026-09-10 15:13:07 +0200

    Add settings page to configure server

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:material_ui/material_ui.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MainApp());
@@ -49,6 +50,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<List<OpenTonicList>> futureLists;
+  late SharedPreferences prefs;
+  late bool loaded = false;
 
   @override
   void initState() {
@@ -56,19 +59,42 @@ class _HomePageState extends State<HomePage> {
     futureLists = fetchLists();
   }
 
+  Future<void> loadPrefs() async {
+    final prefsLocal = await SharedPreferences.getInstance();
+    setState(() {
+      prefs = prefsLocal;
+      loaded = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("OpenTonic")),
+      appBar: AppBar(
+        title: const Text("OpenTonic"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
       body: FutureBuilder(
         future: futureLists,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             var listItems = snapshot.data!.map((list) {
-              return ListTile(
-                title: Text(list.name),
-                subtitle: Text(list.owner),
-                leading: Icon(Icons.storefront),
+              return Card(
+                child: ListTile(
+                  title: Text(list.name),
+                  subtitle: Text(list.owner),
+                  leading: Icon(Icons.storefront),
+                ),
               );
             }).toList();
             return ListView(children: listItems);
@@ -79,18 +105,124 @@ class _HomePageState extends State<HomePage> {
           return const CircularProgressIndicator();
         },
       ),
-      // ListTile(
-      //   title: Text("Jumbo"),
-      //   subtitle: Text("Mama"),
-      //   leading: Icon(Icons.storefront),
-      // ),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  late bool loaded = false;
+  late SharedPreferences prefs;
+  final serverUrlContoller = TextEditingController();
+  final usernameContoller = TextEditingController();
+  final passwordContoller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    loadPrefs();
+  }
+
+  Future<void> loadPrefs() async {
+    final prefsLocal = await SharedPreferences.getInstance();
+    setState(() {
+      prefs = prefsLocal;
+      loaded = true;
+      serverUrlContoller.text = prefs.getString("server-url") ?? "";
+      usernameContoller.text = prefs.getString("username") ?? "";
+      passwordContoller.text = prefs.getString("password") ?? "";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!loaded) {
+      return Scaffold(
+        appBar: AppBar(title: Text("Settings")),
+        body: CircularProgressIndicator(),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Settings")),
+      body: ListView(
+        padding: EdgeInsetsGeometry.all(16),
+        children: [
+          Column(
+            spacing: 16,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Server Settings", style: TextStyle(fontSize: 24)),
+              Card.filled(
+                child: Container(
+                  padding: EdgeInsetsGeometry.all(16),
+                  child: Column(
+                    spacing: 16,
+                    children: [
+                      TextField(
+                        controller: serverUrlContoller,
+                        decoration: InputDecoration(
+                          labelText: "Server URL",
+                          border: OutlineInputBorder(),
+                          hintText: "https://server.example:port/subdirectory",
+                        ),
+                        autocorrect: false,
+                        keyboardType: TextInputType.url,
+                        onSubmitted: (value) {
+                          prefs.setString("server-url", value);
+                        },
+                      ),
+                      TextField(
+                        controller: usernameContoller,
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          border: OutlineInputBorder(),
+                          hintText: "admin",
+                        ),
+                        autocorrect: false,
+                        keyboardType: TextInputType.name,
+                        onSubmitted: (value) {
+                          prefs.setString("username", value);
+                        },
+                      ),
+                      TextField(
+                        controller: passwordContoller,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(),
+                          hintText: "password",
+                        ),
+                        autocorrect: false,
+                        keyboardType: TextInputType.text,
+                        onSubmitted: (value) {
+                          prefs.setString("password", value);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 Future<List<OpenTonicList>> fetchLists() async {
-  const serverUrl = "http://10.0.2.2:8080/opentonic";
-  const authToken = "dGhpanM6dGhpanM=";
+  final prefs = await SharedPreferences.getInstance();
+  final serverUrl = prefs.getString("server-url");
+  final username = prefs.getString("username");
+  final password = prefs.getString("password");
+  final authToken = base64Encode(utf8.encode("$username:$password"));
   final response = await http.get(
     Uri.parse("$serverUrl/api/lists"),
     headers: {HttpHeaders.authorizationHeader: "Basic $authToken"},
